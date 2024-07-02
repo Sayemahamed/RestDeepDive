4308d397e3d3d83d12369b4d641e66b34cc969e1
Sayem <95583680+Sayemahamed@users.noreply.github.com>
2024-07-02 21:02:52 +0600

    doing a little testing

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField(blank=False, null=False)
+    stock = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+
+    @property
+    def sale_price(self):
+        return "%.2f" %(float(self.price) * 0.8)
+    def __str__(self):
+        return self.name
--- py_client/main.py
+++ py_client/main.py
@@ -1,6 +1,6 @@
 import requests
 # endpoint="https://httpbin.org/status/200"
-endpoint="http://localhost:8000/"
+endpoint="http://localhost:8000/api/product/?format=json"
 
 response=requests.get(endpoint)
 print(response.text)
