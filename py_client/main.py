cb0ce1c4ddff836c399751e7028b06f258e791cd
Sayem <95583680+Sayemahamed@users.noreply.github.com>
2024-07-02 14:15:54 +0600

    created api app

--- py_client/main.py
+++ py_client/main.py
@@ -1,7 +1,7 @@
 import requests
-endpoint="https://httpbin.org/status/200"
-endpoint="https://httpbin.org/anything"
+# endpoint="https://httpbin.org/status/200"
+endpoint="http://localhost:8000/"
 
-response=requests.get(endpoint ,json={"say":"hi"})
+response=requests.get(endpoint)
 print(response.text)
 print(response.status_code)
