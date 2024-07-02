57c289ecbd1ee818ade81b84906db47a7db0590f
Sayem <95583680+Sayemahamed@users.noreply.github.com>
2024-07-02 14:09:19 +0600

    client ok for now

--- py_client/main.py
+++ py_client/main.py
@@ -2,5 +2,6 @@ import requests
 endpoint="https://httpbin.org/status/200"
 endpoint="https://httpbin.org/anything"
 
-response=requests.get(endpoint)
+response=requests.get(endpoint ,json={"say":"hi"})
 print(response.text)
+print(response.status_code)
